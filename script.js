0e03d0bccfb6408fa8c5536ba9d910f7e67a88fc
luigicara <luigi.caravelli98@gmail.com>
2022-11-04 16:22:07 +0100

    complete

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstName = prompt('Inserisci il tuo nome');
+
+let lastName = prompt('Inserisci il tuo cognome');
+
+let color = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById("title").innerHTML = `La tua password è ${firstName}${lastName}${color}21`
